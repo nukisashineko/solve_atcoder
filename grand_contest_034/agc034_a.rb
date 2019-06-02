0dade05a50a994a56bbb90ad73f24afe50a83606
nukisashineko <nukisashineko@gmail.com>
2019-06-03 00:02:08 +0900

    何もわからん

---
+++ grand_contest_034/agc034_a.rb
@@ -0,0 +1,48 @@
+
+
+class Agc034A
+  def self.input()
+    _, a, b, c, d = STDIN.gets.to_s.chomp.split(/ /)
+    line_map = STDIN.gets()
+    [a.to_i, b.to_i, c.to_i, d.to_i, line_map]
+  end
+
+  def self.output(result)
+    puts (result ? "Yes": "No")
+  end
+
+  def self.non_duplicated_zone_check(start_i, end_i, line_map)
+    part_of_line_map = line_map[start_i..end_i]
+    line_map[start_i] == '.' && line_map[end_i] == '.' && !part_of_line_map.match(/##/)
+  end
+
+  def self.duplicated_zone_check(start_i, end_i, line_map, need_reverse)
+    part_of_line_map = line_map[start_i..end_i]
+    non_duplicated_zone_check(start_i, end_i, line_map) &&
+        (!need_reverse || !!part_of_line_map.match(/\.\.\./))
+  end
+
+  def self.have_duplicated_zone_calc(a,b,c,d,line_map)
+    duplicated_zone_check([a,b].min,[c,d].max,line_map, d < c)
+  end
+
+  def self.calc(a, b, c, d, line_map)
+    a_i = a - 1
+    b_i = b - 1
+    c_i = c - 1
+    d_i = d - 1
+    return non_duplicated_zone_check(a_i,c_i,line_map) && non_duplicated_zone_check(b_i,d_i,line_map) if (c_i <= b_i)
+    return have_duplicated_zone_calc(a_i, b_i, c_i, d_i,line_map)
+  end
+
+  def self.main()
+    a, b, c, d, line_map = input()
+    result = calc(a,b,c,d,line_map)
+    output(result)
+  end
+end
+
+if __FILE__ == $0
+  Agc034A.main
+  exit
+end
---
+++ grand_contest_034/agc034_a_spec.rb
@@ -0,0 +1,74 @@
+require 'rspec'
+require_relative './agc034_a'
+
+describe Agc034A do
+  context 'non_duplicated_zone_check' do
+    it 'All or Nothing' do
+      expect(Agc034A.non_duplicated_zone_check(0,9,'..........')).to be_truthy
+      expect(Agc034A.non_duplicated_zone_check(0,9,'.#########')).to be_falsey
+    end
+    it 'can not move' do
+      expect(Agc034A.non_duplicated_zone_check(0,9,'.##.......')).to be_falsey
+    end
+    it 'selected only one way' do
+      expect(Agc034A.non_duplicated_zone_check(0,10,'.#.#.#.#.#.')).to be_truthy
+    end
+  end
+
+  context'duplicated_zone_check' do
+    context 'not need reverse' do
+      it 'All or Nothing' do
+        expect(Agc034A.duplicated_zone_check(0,9,'..........', false)).to be_truthy
+        expect(Agc034A.duplicated_zone_check(0,9,'.########.', false)).to be_falsey
+      end
+      it 'can not move' do
+        expect(Agc034A.duplicated_zone_check(0,9,'.##.......', false)).to be_falsey
+      end
+      it 'selected only one way' do
+        expect(Agc034A.duplicated_zone_check(0,10,'.#.#.#.#.#.', false)).to be_truthy
+      end
+      it 'can reverse order' do
+        expect(Agc034A.duplicated_zone_check(0,10,'...#.#.#.#.', false)).to be_truthy
+        expect(Agc034A.duplicated_zone_check(0,10,'.#.#.#.#...', false)).to be_truthy
+        expect(Agc034A.duplicated_zone_check(0,10,'.#.#.#...#.', false)).to be_truthy
+      end
+      it 'can not reverse order' do
+        expect(Agc034A.duplicated_zone_check(0,9,'..#.#.#.#.', false)).to be_truthy
+        expect(Agc034A.duplicated_zone_check(0,9,'.#.#.#.#..', false)).to be_truthy
+        expect(Agc034A.duplicated_zone_check(0,9,'.#.#.#..#.', false)).to be_truthy
+      end
+    end
+
+    context 'need reverse' do
+      it 'All or Nothing' do
+        expect(Agc034A.duplicated_zone_check(0,9,'..........', true)).to be_truthy
+        expect(Agc034A.duplicated_zone_check(0,9,'.#########', true)).to be_falsey
+      end
+      it 'can not move' do
+        expect(Agc034A.duplicated_zone_check(0,9,'.##.......', true)).to be_falsey
+      end
+      it 'selected only one way' do
+        expect(Agc034A.duplicated_zone_check(0,10,'.#.#.#.#.#.', true)).to be_falsey
+      end
+      it 'can reverse order' do
+        expect(Agc034A.duplicated_zone_check(0,10,'...#.#.#.#.', true)).to be_truthy
+        expect(Agc034A.duplicated_zone_check(0,10,'.#.#.#.#...', true)).to be_truthy
+        expect(Agc034A.duplicated_zone_check(0,10,'.#.#.#...#.', true)).to be_truthy
+      end
+      it 'can not reverse order' do
+        expect(Agc034A.duplicated_zone_check(0,9,'..#.#.#.#.', true)).to be_falsey
+        expect(Agc034A.duplicated_zone_check(0,9,'.#.#.#.#..', true)).to be_falsey
+        expect(Agc034A.duplicated_zone_check(0,9,'.#.#.#..#.', true)).to be_falsey
+      end
+    end
+  end
+
+  describe 'sample test' do
+    it 'sample 1' do
+      expect(Agc034A.calc(1,3,7,6, '.#..#..')).to be_falsey
+    end
+    it 'sample 2' do
+      expect(Agc034A.calc(1,3,15,13, '...#.#...#.#...')).to be_truthy
+    end
+  end
+end
